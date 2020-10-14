e6dee63f70ff26931e84f5e245c83c6999241317
vcslav-v <vcslav.v@gmail.com>
2020-10-14 17:02:52 +0300

    add --format arg to argparse

--- DifferencesEvaluator/evaluator.py
+++ DifferencesEvaluator/evaluator.py
@@ -1,11 +1,12 @@
 """Differences evaluator."""
 
 
-def get_diff(first_file: str, second_file: str):
+def get_diff(first_file: str, second_file: str, format: str):
     """Generate diffirences of two files.
 
     Parameters:
         first_file: path to first target file
         second_file: path to second target file
+        format: format needed plain or json 
     """
     pass
--- DifferencesEvaluator/scripts/gendiff.py
+++ DifferencesEvaluator/scripts/gendiff.py
@@ -6,14 +6,17 @@ import argparse
 from DifferencesEvaluator import evaluator
 
 parser = argparse.ArgumentParser(description='Generate diff')
+
 parser.add_argument('first_file')
 parser.add_argument('second_file')
+
+parser.add_argument('-f', '--format', help='set format of output')
 
 args = parser.parse_args()
 
 
 def main():
-    evaluator.get_diff(args.first_file, args.second_file)
+    evaluator.get_diff(args.first_file, args.second_file, args.format)
 
 
 if __name__ == "__main__":
