c8b8dcf728bf963ea8ba3f537a9c0f1c06e6bbfb
vcslav-v <vcslav.v@gmail.com>
2021-01-12 20:45:34 +0400

    add inner representation

--- differences_evaluator/file_parser.py
+++ differences_evaluator/file_parser.py
@@ -1,5 +1,6 @@
 import json
 import os
+from typing import List
 
 import yaml
 
@@ -35,3 +36,34 @@ def get_extension(file_path: str) -> str:
     """
     _, file_extension = os.path.splitext(file_path)
     return file_extension[1:]
+
+
+def get_repr(
+    data: dict,
+    node: str = 'root',
+    path: List['str'] = []
+) -> dict:
+    """Make inner representation tree from data dict.
+
+    Parameters:
+        data: data from file
+        node: name of node
+
+    Returns:
+        representation tree with two type object
+        node - {'node': 'node', 'leafs': [], 'children':[], 'path':()}
+        leaf - {'key':'setting1', 'value': 'Value 1', 'path':()}
+    """
+    result = {'node': node, 'leafs': [], 'children': [], 'path': path}
+
+    new_path = path.copy()
+    new_path.append(node)
+
+    for key, value in data.items():
+        if type(value) is dict:
+            result['children'].append(get_repr(value, key, new_path))
+        else:
+            result['leafs'].append(
+                {'key': key, 'value': value, 'path': new_path}
+            )
+    return result
