e6ef323a59717326d49b8c95ee5a9a4457539c47
vcslav-v <vcslav.v@gmail.com>
2021-01-19 20:10:58 +0300

    fix

--- gendiff/file_parser.py
+++ gendiff/file_parser.py
@@ -1,4 +1,5 @@
 import json
+import os
 import pathlib
 
 import yaml
@@ -13,7 +14,7 @@ def get_data(file_path: pathlib.Path) -> dict:
     Returns:
         data from file in dict
     """
-    file_extension = file_path.suffix.lower()
+    file_extension = get_extension(file_path).lower()
     with open(file_path, 'r') as file_data:
         if file_extension == '.json':
             data = json.load(file_data)
@@ -24,3 +25,16 @@ def get_data(file_path: pathlib.Path) -> dict:
                 'File "{path}" is not json or yaml file'.format(path=file_path)
             )
     return data
+
+
+def get_extension(file_path: pathlib.Path) -> str:
+    """Parse file extention from file path.
+
+    Parameters:
+        file_path: path to file
+
+    Returns:
+        file extention
+    """
+    _, file_extension = os.path.splitext(file_path)
+    return file_extension[1:]
